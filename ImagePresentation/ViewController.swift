e88cf260857273a4cdf477d1c64e692281fddedc
Alexey Gaidykov <swiftgaidaalex@gmail.com>
2022-12-13 19:20:51 +0300

    Initial Commit

---
+++ ImagePresentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImagePresentation
+//
+//  Created by Alexey Gaidykov on 13.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
